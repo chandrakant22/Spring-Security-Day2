14da62d2a4aacf9cfb73c09d19685193c7579d7a
chandrakant sir <cakant70@gmail.com>
2023-04-10 12:42:54 +0530

    day 2

---
+++ src/main/java/com/example/demo/SpringJspSecurityDay2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJspSecurityDay2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJspSecurityDay2Application.class, args);
+	}
+
+}
